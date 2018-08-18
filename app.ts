e833e3197c0997bfbdd7de0e4532ccb20f2ad91f
Steven Tang <steven.yc.tang@gmail.com>
2018-08-18 15:00:26 +1000

    Fix various bugs
    Fix first run of 'npm run dev'
    Adjust helmet placement
    
    Use concurrently for npm run dev on Windows
    
    Add pre-commit hook package

--- app.ts
+++ app.ts
@@ -11,12 +11,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Enable helmet, which sets security headers
+app.use(helmet());
+
 // Init routes
 import router from './routes';
 app.use('/', router);
-
-// Enable helmet, which sets security headers
-app.use(helmet());
 
 // Catch 404 and forward to error handler
 app.use(function(req: Request, res: Response, next: NextFunction) {
